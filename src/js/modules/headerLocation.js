45d9dc73cae6b72dedfcc4b1c9238efe50244323
Sayko003 <konstantin.web@bk.ru>
2025-04-13 14:31:39 +0300

    add main pages dekstop

--- src/js/modules/headerLocation.js
+++ src/js/modules/headerLocation.js
@@ -1,13 +1,24 @@
 const headerLocation = () =>{
-    const btnLink = document.querySelector('.btn--location'),
-        block = document.querySelector('.header--block-location'),
+    const btnLinkHeader = document.querySelector('.header-btn--location'),
+        blockHeader = document.querySelector('.header--block-location'),
+        btnLinkFooter = document.querySelector('.footer-btn--location'),
+        blockFooter = document.querySelector('.footer--block-location'),
         body = document.querySelector('body');
 
-    btnLink.addEventListener(`click`, (e) =>{
-        block.classList.toggle('header--block-location-active')
+    // btnLink.addEventListener(`click`, (e) =>{
+    //     block.classList.toggle('header--block-location-active')
        
-    })
+    // })
+    function showLocation(btn,block,activeClass){
+        btn.addEventListener(`click`, (e) =>{
+            e.preventDefault();
 
+            block.classList.toggle(activeClass);
+        })
+    }
+
+    showLocation(btnLinkHeader,blockHeader, `header--block-location-active`);
+    showLocation(btnLinkFooter,blockFooter, `footer--block-location-active`);
 }
 
 export default headerLocation;
